3d7b07a1ba8c922228cddb7b004c9a7dd79456da
Jeremy Karlsson <jeremy@barlingshult.se>
2025-08-27 18:55:18 +0200

    Update method name in tests

--- test/watchColorSchemeChange.test.ts
+++ test/watchColorSchemeChange.test.ts
@@ -1,16 +1,16 @@
 import { expect, suite, test } from "vitest";
-import { watchColorScheme } from "../src/main";
+import { watchColorSchemeChange } from "../src/main";
 
-suite("watchColorScheme", () => {
+suite("watchColorSchemeChange", () => {
   test("adds default scheme to host if no initial scheme is provided", () => {
     const host = document.createElement("div");
-    watchColorScheme(host);
+    watchColorSchemeChange(host);
     expect(host.classList.contains("scheme-default")).toBe(true);
   });
 
   test("should add initial scheme class to host if provided", () => {
     const host = document.createElement("div");
-    watchColorScheme(host, "dark");
+    watchColorSchemeChange(host, "dark");
     expect(host.classList.contains("scheme-dark")).toBe(true);
   });
 });
